4d3afe0036735f8b2351cf97961523caee52ead3
P0646084 <andre.tancredo@portoseguro.com.br>
2022-11-16 16:11:30 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,6 +1,6 @@
 package com.devsuperior.dsmeta.repositories;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.time.LocalDate;
 
 import org.springframework.data.domain.Page;
@@ -13,6 +13,6 @@ import com.devsuperior.dsmeta.model.Sale;
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
-//	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
-//	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
