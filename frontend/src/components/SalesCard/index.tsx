f6712fd3404cfd8015b4ba14c3084fbf1228b5e5
P0646084 <andre.tancredo@portoseguro.com.br>
2022-11-18 09:29:08 -0300

    Axios, useEffect first request

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -4,7 +4,8 @@ import "react-datepicker/dist/react-datepicker.css";
 
 import './style.css';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function SalesCard() {
 
@@ -13,6 +14,13 @@ function SalesCard() {
 
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/sales")
+            .then(response => {
+                console.log(response.data);
+            })
+    }, []);
 
 
     return (
